d3888c969a2b1724dac7fa096e442b715133374c
Panesar <ManpritSingh.Panesar@sony.com>
2025-06-09 21:44:58 +0530

    checked the filterPullRequest

--- src/org/ci/GitHubManager.groovy
+++ src/org/ci/GitHubManager.groovy
@@ -95,12 +95,7 @@ class GitHubManager implements Serializable {
             .replaceAll(/(\r\n|\n|\r)/, '\\\\n')
         def payload = "{ \"body\": \"${escapedMessage}\" }"
         
-        def curlCommand = """curl -s -X POST \
-            -H "Authorization: token ${token}" \
-            -H "Accept: application/vnd.github.v3+json" \
-            -d "${payload}" \
-            https://api.github.com/repos/${repo}/issues/${prNumber}/comments \
-            -w '%{http_code}' -o output.txt"""
+        def curlCommand = """curl -s -X POST \ -H "Authorization: token ${token}" \ -H "Accept: application/vnd.github.v3+json" \ -d "${payload}" \ https://api.github.com/repos/${repo}/issues/${prNumber}/comments \ -w '%{http_code}' -o output.txt"""
         
         script.echo "curl command: ${curlCommand}"
         
